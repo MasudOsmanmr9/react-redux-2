c69219a80a0ff0673c743fa0b7b569948b33437b
masudosman <masudosmanmr9@gmail.com>
2020-06-05 02:22:30 +0600

    compleated project

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,23 +1,33 @@
 import React,{Component} from 'react'
-import axios from 'axios'
+import {connect} from 'react-redux'
+// import axios from 'axios'
 class Post extends Component{
-    state={
-        post:null    
-    }
-    componentDidMount(){
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.post_id}`)
-        .then((res)=>{
-            console.log(res)
-            this.setState({
-                post:res.data
-            })
-        })
+    // state={
+    //     post:null    
+    // }
+    // componentDidMount(){
+    //     axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.post_id}`)
+    //     .then((res)=>{
+    //         console.log(res)
+    //         this.setState({
+    //             post:res.data
+    //         })
+    //     })
+    // }
+    handelClick = (id)=>{
+        this.props.deletePost(id)
+        this.props.history.push('/')
     }
     render(){
-        const post = this.state.post?(
+        // const post = this.state.post?(
+            console.log(this.props)
+        const post = this.props.post?(
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p className="center">{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={()=>{this.handelClick(this.props.post.id)}}>Delete Post</button>
+                </div>
             </div>
         ):(
             <h1>loading...</h1>
@@ -31,4 +41,18 @@ class Post extends Component{
     }
 }
 
-export default Post
+const mapStateToProps = (state,ownProps)=>{
+    let id = ownProps.match.params.post_id;
+    return{
+        post : state.posts.find(post => post.id === id)
+    }
+
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        deletePost : (id)=>{ dispatch({type:'DELETE_POST',id:id})}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Post)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,23 @@
 const initState = {
-    posts : []
+    posts : [
+        {id:'1',title:'aStala bista',body:'spanish language may be ,lets talk something new (a+b)^2 = a^2+2ab+b^2'},
+        {id:'2',title:'hamb burger',body:'its a great burger, its so delicious that you will close your eyes just yummy'},
+        {id:'3',title:'Sausage',body:'sausage looks like a roll, it\'s a blunder of meat '},
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
+    console.log('action from root reducer',action)
+    if(action.type === 'DELETE_POST'){
+        let newPost = state.posts.filter(post =>{
+            return action.id !== post.id
+        })
+        console.log('state.posts',state.posts)
+        return{
+            ...state,
+            posts:newPost
+        }
+    }
     return state
 }
 
